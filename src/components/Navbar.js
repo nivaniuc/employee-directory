fad066540a51924af525460f830c2c7b14c52709
ant <nivaniucc@gmail.com>
2021-05-14 22:53:05 -0400

    After further consideration, I have decided to veto the TODO for the placeholder image.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,9 @@ import Search from "./Search"
 function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex justify-content-between">
-        {/* TODO: Add placeholder and folder before deploying */}
-        <img src="/images/employeeIcon.PNG" alt="logo"/>
-        {/* Me: How did this break?! 
-            Also Me: -never added the placeholder image- */}
         <a className="navbar-brand" href="/">
         Employee Directory
         </a>
-        import Search from "./Search"
         <Search value={props.value} handleChange={props.handleChange} handleSubmit={props.handleSubmit}/>
       </nav>
   );
